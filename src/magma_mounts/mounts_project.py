4d2c8ed78c624019b7b34b1c94bbb151a9cdb4a8
Martanto <martanto@live.com>
2024-11-01 05:05:44 +0700

    Update mounts_project.py
    - refactor json
    - add magma volcano code
    - change code to smithsonian_id

--- src/magma_mounts/mounts_project.py
+++ src/magma_mounts/mounts_project.py
@@ -9,51 +9,63 @@ from requests import Response
 volcanoes: List[Dict[str, Any]] = [
     {
         "name": "Lewotobi Laki-laki",
-        "code": 264180,
+        "smithsonian_id": "264180",
+        "code": "LWK",
     },
     {
         "name": "Marapi",
-        "code": 261140,
+        "smithsonian_id": "261140",
+        "code": "MAR",
     },
     {
         "name": "Anak Krakatau",
-        "code": 262000,
+        "smithsonian_id": "262000",
+        "code": "KRA",
     },
     {
         "name": "Kerinci",
-        "code": 261170,
+        "smithsonian_id": "261170",
+        "code": "KER",
     },
     {
         "name": "Karangetang",
-        "code": 267020,
+        "smithsonian_id": "267020",
+        "code": "KAR",
     },
     {
         "name": "Dukono",
-        "code": 268010,
+        "smithsonian_id": "268010",
+        "code": "DUK",
     },
     {
         "name": "Ili Lewotolok",
-        "code": 264230,
+        "smithsonian_id": "264230",
+        "code": "LEW",
     },
     {
         "name": "Ibu",
-        "code": 268030,
+        "smithsonian_id": "268030",
+        "code": "IBU",
     },
     {
         "name": "Semeru",
-        "code": 263300,
+        "smithsonian_id": "263300",
+        "code": "SMR",
     },
     {
         "name": "Raung",
-        "code": 263340,
+        "smithsonian_id": "263340",
+        "code": "RAU",
     },
     {
         "name": "Ijen",
-        "code": 263350,
+        "smithsonian_id": "263350",
+        "code": "IJE",
     },
     {
         "name": "Slamet",
-        "code": 263180
+        "smithsonian_id": "263180",
+        "code": "SLA",
     }
 ]
 
@@ -62,7 +74,7 @@ class MountsProject:
     URL: str = "http://mounts-project.com/timeseries/"
 
     def __init__(self, include_anomaly: bool = False, filter_value: float = 0.1,
-                 output_directory: str = None):
+                 output_directory: str = None, save_to_database: bool = True):
         self.include_anomaly = include_anomaly
         self.filter_value = filter_value
 
@@ -73,6 +85,8 @@ class MountsProject:
 
         self.json_dir: str = os.path.join(self.output_directory, 'json')
         os.makedirs(self.json_dir, exist_ok=True)
+
+        self.save_to_database = save_to_database
 
         self.volcanoes: List[Dict[str, Any]] = volcanoes
 
@@ -87,35 +101,36 @@ class MountsProject:
             Dict[str, Any]: JSON from javascript.
         """
         var_graph = re.search(r"(?:^|\s|;)var\s+graph\s*=\s*([^']+})", text)
+        assert var_graph is not None, f"❌ Data not found."
         string_graph = var_graph.group(1)
         json_graph = json.loads(string_graph)
         return json_graph
 
     @staticmethod
-    def fetch(volcano_code: str) -> Dict[str, Any]:
+    def fetch(smithsonian_id: str) -> Dict[str, Any]:
         """Fetch JSON from URL.
 
         Args:
-            volcano_code (str): Volcano code.
+            smithsonian_id (str): Volcano smithsonian_id.
 
         Returns:
             Dict[str, Any]: JSON from URL.
         """
-        url = f"{MountsProject.URL}{volcano_code}"
+        url = f"{MountsProject.URL}{smithsonian_id}"
         response: Response = requests.get(url)
         return MountsProject.get_json_from_javascript(response.text)
 
-    def write_json(self, volcano_code: str) -> List[Dict[str, Any]]:
+    def write_json(self, smithsonian_id: str) -> List[Dict[str, Any]]:
         """Write JSON to file.
 
         Args:
-            volcano_code (str): Volcano code.
+            smithsonian_id (str): Volcano smithsonian_id.
 
         Returns:
             List[Dict[str, Any]]: List of dict from URL.
         """
-        json_file = os.path.join(self.json_dir, f"{volcano_code}.json")
-        graph_json = self.fetch(volcano_code)
+        json_file = os.path.join(self.json_dir, f"{smithsonian_id}.json")
+        graph_json = self.fetch(smithsonian_id)
         try:
             with open(json_file, "w") as write_file:
                 json.dump(graph_json['data'], write_file, indent=2)
@@ -124,14 +139,12 @@ class MountsProject:
         except Exception as e:
             print(f"{e}")
 
-    def values_to_df(self, values: Dict[str, Any], volcano_code: str,
-                     volcano_name: str, value_type: str = 'SO2') -> pd.DataFrame:
+    def values_to_df(self, values: Dict[str, Any], volcano: Dict[str, str], value_type: str = 'SO2') -> pd.DataFrame:
         """Values to dataframe.
 
         Args:
             values (Dict[str, Any]): Values of SO2 or Thermal.
-            volcano_code (str): Volcano code.
-            volcano_name (str): Volcano name.
+            volcano (Dict[str, str): Volcano name, code, and smithsonian_id
             value_type (str): Value type. Defaults to 'SO2'.
 
         Returns:
@@ -139,11 +152,16 @@ class MountsProject:
         """
         df = pd.DataFrame.from_dict(values)
         df['datetime'] = pd.to_datetime(df['datetime'])
-        df['date'] = df['datetime'].apply(lambda x: x.strftime("%Y-%m-%d"))
-        df['time'] = df['datetime'].apply(lambda x: x.strftime("%H:%M:%S"))
+        df['date'] = df['datetime'].apply(lambda _datetime: _datetime.strftime("%Y-%m-%d"))
+        df['time'] = df['datetime'].apply(lambda _datetime: _datetime.strftime("%H:%M:%S"))
         df['type'] = value_type
-        df['code'] = volcano_code
-        df['volcano_name'] = volcano_name
+        df['name'] = volcano['name']
+        df['code'] = volcano['code']
+        df['smithsonian_id'] = volcano['smithsonian_id']
+
+        # Rearrange columns
+        df = df.loc[:, ['datetime', 'date', 'time', 'type', 'name', 'code', 'smithsonian_id', 'value', 'image']]
+
         df.set_index('datetime', inplace=True)
 
         if self.include_anomaly is False:
@@ -218,13 +236,14 @@ class MountsProject:
             None
         """
         for index, volcano in enumerate(self.volcanoes):
-            volcano_code = volcano['code']
-            volcano_name = volcano['name']
-            print(f"🌋 Extracting {volcano_name} volcano.")
-            data = self.write_json(volcano_code)
+            print(f"🌋 Extracting {volcano['name']} volcano.")
+            data = self.write_json(volcano['smithsonian_id'])
+
             so2_df: pd.DataFrame = self.values_to_df(
-                self.get_so2_values(data), volcano_code, volcano_name)
+                self.get_so2_values(data), volcano=volcano)
             thermal_df: pd.DataFrame = self.values_to_df(
-                self.get_thermal_values(data), volcano_code, volcano_name, value_type='Thermal')
-            self.save(so2_df, volcano_name)
-            self.save(thermal_df, volcano_name, value_type='thermal')
+                self.get_thermal_values(data), volcano=volcano, value_type='Thermal')
+
+            self.save(so2_df, volcano['name'])
+            self.save(thermal_df, volcano['name'], value_type='thermal')
+        print(f"\n✅ Done!")
